f997c20a846bb566593c68c4814c40d8d1117105
igorpsantos <47090196+igorpsantos@users.noreply.github.com>
2019-03-30 22:07:36 -0300

    escopo.cpp
    escopo da classe pai e atributos que serão utilizados

---
+++ escopo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class perfil{
+  string nome;
+  char genero;
+  int idade;
+  string local;
+  string cinema;
+  string musica;
+  string hobbies;
+  string curso;
+  public:
+    
+};
