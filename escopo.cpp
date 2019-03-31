603c9b15fff1ae464cfc7d112e7f8c11e604109f
igorpsantos <47090196+igorpsantos@users.noreply.github.com>
2019-03-31 19:55:34 -0300

    Update escopo.cpp

--- escopo.cpp
+++ escopo.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 class perfil{
+  int idPerfil;
   string nome;
+  string email; // validacao para criacao de perfil a partir do email da faculdade
   char genero;
   int idade;
   string local;
@@ -12,5 +14,26 @@ class perfil{
   string hobbies;
   string curso;
   public:
+    perfil(); // construtor
+    mostra_perfil();
+    edita_perfil();
+    exclui_perfil();
     
 };
+
+class atributos{
+  int atb1,atb2,atb3,atb4,atb5,atb6;
+  public:
+    atributos(perfil a); // recebe como parametro o perfil de um usuario
+    int getAproximacaoUsuario();
+    void imprime_relatorio(int idperfil);
+};
+
+void learquivo(file *arquivo);
+void menu();
+void display();
+
+int main(){
+  return 0;
+}
+
