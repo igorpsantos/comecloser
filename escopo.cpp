b0b2cb88c78df1e60b92f049c892f972e27d6e22
igorpsantos <47090196+igorpsantos@users.noreply.github.com>
2019-05-03 23:00:26 -0300

    Update escopo.cpp

--- escopo.cpp
+++ escopo.cpp
@@ -19,15 +19,37 @@ class perfil{
 };
 
 class Atributos : perfil{
-  int atb1,atb2,atb3,atb4;
+  int atb1,atb2,atb3,atb4; // valor inteiro de um atributo para definir valores
   public:
     Atributos(perfil a); // recebe como parametro o perfil de um usuario
     
 };
 
+// A FUNCAO LE ARQUIVO DEVE ARMAZENAR NOS PERFIS OS DADOS LIDOS DIRETAMENTE DO ARQUIVO, TAMBEM NA CRIAÇÃO DO PERFIL DE QUEM FAZ A BUSCA
+// DEVE OBSERVAR QUE OS MESMOS PARAMETROS ESCOLHIDOS PARA OS ATRIBUTOS/CRITERIOS E/OU INTERESSES SERÃO OS MESMOS QUE FORAM CRIADO NO ARQUIVO
+// OU TAMBEM CRIAR UMA CLASSE AONDE ESSA SERIA A CLASSE PAI QUE CRIARIA O "BANCO DE DADOS DE USUARIOS" UTILIZANDO A CLASSE FILHA "PERFIL"
+// PARA ARMAZENAR OS DEMAIS...
 void learquivo(file *arquivo);
-void menu();
-void display();
+
+void menu(int operacao){
+  /* OPCOES DO MENU
+  *  CRIAR PERFIL
+  *  BUSCAR PERFIL
+  *  ATUALIZAR PERFIL
+  */
+  switch(operacao){
+    case 0: // criar
+      break;
+    case 1: // buscar perfil
+      break;
+    case 2: // atualizar
+      break;
+    DEFAULT:
+      //OPERACAO INVALIDA? LOOP ?
+  }    
+}
+
+void display(); // nome do software - COMECLOSER -> "Chega mais" tradução ao pé da letra
 
 int main(){
   return 0;
